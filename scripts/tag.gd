7e50d165893532b3b491191ac80db7690a861e82
colmisch123 <colemische@gmail.com>
2026-05-02 02:24:10 -0500

    Added some data collection functions

--- scripts/tag.gd
+++ scripts/tag.gd
@@ -10,6 +10,15 @@ extends Node3D
 var time_elapsed: float = 0.0
 const MAX_TIME: float = 45.0
 
+# Data Collection Variables
+var games_played: int = 0
+var tagger_wins: int = 0
+var evader_wins: int = 0
+
+var total_cumulative_distance: float = 0.0
+var total_frames: int = 0
+var total_time_to_tag: float = 0.0
+
 func _ready() -> void:
 	# Push the selected modes down to the agent controllers as soon as the game loads
 	tagger.get_node("AIController3DTagger").current_reward_mode = tagger_reward_mode
@@ -19,6 +28,11 @@ func _physics_process(delta: float) -> void:
 	time_elapsed += delta
 	
 	var current_dist = tagger.global_position.distance_to(evader.global_position) 
+	
+	# Track data continuously for the average distance metric
+	total_cumulative_distance += current_dist
+	total_frames += 1
+	
 	# Check for Tag
 	if current_dist <= 1.1: 
 		handle_tag()
@@ -27,21 +41,34 @@ func _physics_process(delta: float) -> void:
 	elif time_elapsed >= MAX_TIME:
 		handle_timeout()
 		
-	if evader.has_any_raycast_collided():
-		
-	
-	evader.raycasts_colliding.clear()
+	if evader.has_method("has_any_raycast_collided") and evader.has_any_raycast_collided():
+		pass
+	if "raycasts_colliding" in evader:
+		evader.raycasts_colliding.clear()
 
 func handle_tag():
+	# Update Metrics for Tagger Win
+	tagger_wins += 1
+	games_played += 1
+	total_time_to_tag += time_elapsed
+	
 	# Reward seeker, punish runner
 	tagger.get_node("AIController3DTagger").reward += 15.0
 	evader.get_node("AIController3DEvader").reward -= 15.0
+	
+	log_statistics()
 	reset_arena()
 
 func handle_timeout():
+	# Update Metrics for Evader Win
+	evader_wins += 1
+	games_played += 1
+	
 	# Punish seeker, reward runner
 	tagger.get_node("AIController3DTagger").reward -= 15.0
 	evader.get_node("AIController3DEvader").reward += 15.0
+	
+	log_statistics()
 	reset_arena()
 
 func reset_arena():
@@ -52,3 +79,21 @@ func reset_arena():
 	# Inform Python the episode ended so it can restart the environment
 	tagger.get_node("AIController3DTagger").reset()
 	evader.get_node("AIController3DEvader").reset()
+
+# Data Output Function
+func log_statistics():
+	var tagger_win_rate = (float(tagger_wins) / float(games_played)) * 100.0
+	var evader_win_rate = (float(evader_wins) / float(games_played)) * 100.0
+	var avg_dist = total_cumulative_distance / float(total_frames)
+	
+	var avg_tag_time = 0.0
+	if tagger_wins > 0:
+		avg_tag_time = total_time_to_tag / float(tagger_wins)
+		
+	# Print statements for data
+	print("\n--- ROUND ", games_played, " COMPLETE ---")
+	print("Tagger Win Rate: ", str(tagger_win_rate).pad_decimals(2), "%")
+	print("Evader Win Rate: ", str(evader_win_rate).pad_decimals(2), "%")
+	print("Average Distance: ", str(avg_dist).pad_decimals(2), " meters")
+	if tagger_wins > 0:
+		print("Average Time to Tag: ", str(avg_tag_time).pad_decimals(2), " seconds")
